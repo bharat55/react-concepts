acbaf152445337530c064718145d6d0eb79abe1f
Bharat Rathor <bharat.railsdev@gmail.com>
2025-05-04 10:52:21 +0530

    counter functionality

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -57,22 +57,8 @@ function Navbar() {
                                     data-dropdown-toggle="dropdownNavbar"
                                     className="flex items-center justify-between w-full py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:w-auto dark:text-white md:dark:hover:text-blue-500 dark:focus:text-white dark:border-gray-700 dark:hover:bg-gray-700 md:dark:hover:bg-transparent"
                                 >
-                                    Dropdown{" "}
-                                    <svg
-                                        className="w-2.5 h-2.5 ms-2.5"
-                                        aria-hidden="true"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        fill="none"
-                                        viewBox="0 0 10 6"
-                                    >
-                                        <path
-                                            stroke="currentColor"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth={2}
-                                            d="m1 1 4 4 4-4"
-                                        />
-                                    </svg>
+                                    Counter
+                                  
                                 </button>
                                 <div
                                     id="dropdownNavbar"
